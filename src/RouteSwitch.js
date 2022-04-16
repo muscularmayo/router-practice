f75580b23736b3dc3db5ada8b7fa5fc2a400e051
Chris Risden <christopherrisden@gmail.com>
2022-04-16 13:45:18 -0700

    got some links and got some routes for the links to lead to.. pretty cool!

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -3,14 +3,38 @@ import React from 'react';
 import App from './App.js';
 import Profile from './Profile.js';
 import DJKhaled from './DJKhaled.js';
+import Tweet from './Tweet.js'
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
+      <div>
+        <a
+          href="/profile"
+        >
+          Profile
+        </a>
+      </div>
+      <div>
+        <a
+          href="/djkhaled"
+        >
+          DJ KHALED
+        </a>
+      </div>
+      <div>
+        <a
+          href="/tweet"
+        >
+          Tweet
+        </a>
+      </div>
+
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/djkhaled" element={<DJKhaled />} />
+        <Route path="/tweet" element={<Tweet />} />
       </Routes>
     </BrowserRouter>
   )
